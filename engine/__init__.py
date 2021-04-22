df688f663b585992b7b0176eeac0ef1f5b515807
Himanshu Mishra <himanshu210798@gmail.com>
2021-04-22 01:10:20 -0700

    This is the initial commit for the engine of the system. This commit ensures that  that the setup for engine is complete. All the code for engine will go into the src directory and to be called in the main module. This main module serves as the entry point for the system. The Web Interface is to call the main method of the main module in the engine package passing it the location of the image and the method will return the status of operation and the processed text. @author Himanshu Mishr.

---
+++ engine/__init__.py
@@ -0,0 +1,4 @@
+"""
+@file __init__.py Top Level Package Intitalization: `Engine`
+@author Himanshu Mishra
+"""
---
+++ engine/tests/__init__.py
@@ -0,0 +1,4 @@
+"""
+@file __init__.py Sub Package Intitalization: `Tests`
+@author Himanshu Mishra
+"""
