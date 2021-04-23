adcef1a0ed55df7993b0491bb059bc75211c653c
SuryanshDwivedi <iamsuryansh46@gmail.com>
2021-04-23 12:56:40 +0530

    message

--- frontend/src/Routes/Contributers/index.jsx
+++ frontend/src/Routes/Contributers/index.jsx
@@ -4,7 +4,83 @@ import styles from "./style.module.css"
 export default ()=>{
     return (
         <React.Fragment>
-            <hi className={styles.heading}>Contributors</hi>
+            {/* <hi classNameName={styles.heading}>Contributors</hi> */}
+            <div className={styles.sec}>
+
+            
+      <div class={styles.hl}>      <div className={styles.cardsList}>
+  
+  <div className={styles.card}>
+    <div className={styles.cardImage}> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
+    <div className={styles.titleWhite}>
+    <h5>Dr.Munesh Chandra </h5>
+      <p>Associate Professor</p>
+      <p>CSE Department, NIT Agartala</p>
+    </div>
+  </div>
+  </div></div>
+  
+            <div class={styles.hl}><div className={styles.cardsList}>
+  
+  <div className={styles.card}>
+    <div className={styles.cardImage}> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
+    <div className={styles.titleWhite}>
+    <h5>Suryansh Dwivedi</h5>
+      <p>17UCS103</p>
+      <p>CSE Department, NIT Agartala</p>
+    </div>
+  </div>
+  
+   
+  
+  </div></div>
+  
+<div class={styles.hl}>
+<div className={styles.cardsList}>
+  
+  <div className={styles.card}>
+    <div className={styles.cardImage}> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
+    <div className={styles.titleWhite}>
+    <h5>Himanshu Mishra</h5>
+      <p>17UCS105</p>
+      <p>CSE Department, NIT Agartala</p>
+    </div>
+  </div>
+  
+   
+  
+  </div>
+</div>
+ <div class={styles.hl}>
+ <div className={styles.cardsList}>
+  
+  <div className={styles.card}>
+    <div className={styles.cardImage}> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
+    <div className={styles.titleWhite}>
+      <h5>Bhawesh Agrawal</h5>
+      <p>17UCS070</p>
+      <p>CSE Department, NIT Agartala</p>
+    </div>
+  </div>
+  
+   
+  
+  </div>
+ </div>
+ <div class={styles.hl}> <div className={styles.cardsList}>
+  
+  <div className={styles.card}>
+    <div className={styles.cardImage}> <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> </div>
+    <div className={styles.titleWhite}>
+    <h5>Ajay Kumar</h5>
+      <p>17UCS095</p>
+      <p>CSE Department, NIT Agartala</p>
+    </div>
+  </div>
+  
+   </div></div>
+  
+  </div>
         </React.Fragment>
     )
 }
